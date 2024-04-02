57f192cece7c08d01f1813b8da8b592fed891373
bykov-mikhael <bykov-mikhael@yandex.ru>
2024-04-03 00:42:43 +0300

    00:42

--- gtests/tests.cpp
+++ gtests/tests.cpp
@@ -2,13 +2,13 @@
 
 #include <static-lib/static.h>
 
-TEST(GetBool, getBool) {
+TEST(GetBool, retBoolResult) {
   int val1{1};
   int val2{2};
   ASSERT_TRUE(examples::getBool(val1, val2));
 }
 
-TEST(GetInt, incorrect_visa) {
+TEST(GetInt, retIntResult) {
   int val1{1};
   int val2{2};
   ASSERT_EQ(examples::getInt(val1, val2), 1);
